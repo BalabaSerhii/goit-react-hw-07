b982df7b6e0912a51cf9b50d8ad1f1cca534e130
Serhii Balaba <balabaserhii@gmail.com>
2024-06-19 20:23:31 +0200

    1

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,27 @@
+import Contact from "../Contact/Contact";
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
+
+
+const ContactList = () => {
+
+    const contacts = useSelector(selectContacts);
+
+    const filter = useSelector(selectNameFilter);
+
+    const visibleContacts = contacts.filter(contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase()));
+
+    return (
+        <ul >
+            {visibleContacts.map(contact => (
+                <li  key={contact.id}>
+                    <Contact data={contact} />
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default ContactList;
